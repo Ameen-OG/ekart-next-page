4350fc9bf9b901606f86923b8fcb96bc83499827
Ameen <ameennk1110@gmail.com>
2026-03-01 12:21:29 +0530

    somthing changed

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,15 @@
 import "../styles/globals.css";
 import Layout from "../components/Layout";
 import { CartProvider } from "../context/CartContext";
-import Head from 'next/head';
+import Head from "next/head";
+import type { AppProps } from "next/app";
 
-export default function App({ Component, pageProps }) {
+export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
         <title>Ekart Next Page Router</title>
         <link rel="icon" href="/favicon.ico" />
-        {/* Add viewport for mobile responsiveness */}
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
 
